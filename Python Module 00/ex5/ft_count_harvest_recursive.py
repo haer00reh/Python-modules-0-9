f59ec845456b8ad977913e640c603ba0ecda99d1
haer00reh <haytamerrehyby@gmail.com>
2026-01-11 20:02:30 +0100

    fixes

--- Python Module 00/ex5/ft_count_harvest_recursive.py
+++ Python Module 00/ex5/ft_count_harvest_recursive.py
@@ -1,11 +1,11 @@
-def recurs(value):
-    if value == 0:
-        return
-    recurs(value - 1)
-    print(f"Day {value}")
-
-
 def ft_count_harvset_recursive():
     value = int(input("Days until harvest: "))
+
+    def recurs(value):
+        if value == 0:
+            return
+        recurs(value - 1)
+        print(f"Day {value}")
+
     recurs(value)
     print("Harvest time!")
