20954013a303bb5791227552a935bf5095fc8b35
haer00reh <haytamerrehyby@gmail.com>
2026-02-11 14:25:20 +0100

    OK

--- Python Module 04/ex4/ft_crisis_response.py
+++ Python Module 04/ex4/ft_crisis_response.py
@@ -1,18 +1,18 @@
 print("=== CYBER ARCHIVES - CRISIS RESPONSE SYSTEM ===\n")
 try:
-    print("CRISIS ALERT: Attempting access to 'lost_archive.txt'...")
-    with open("lost_archive.txt", "r") as crisis_file:
-        pass
-except Exception:
-    print("RESPONSE: Archive not found in storage matrix")
-print("STATUS: Crisis handled, system stable")
-try:
-    print("\nCRISIS ALERT: Attempting access to 'classified_vault.txt'...")
-    with open("classified_vault.txt", "r") as vault_file:
-        pass
-except Exception:
-    print("RESPONSE: Security protocols deny access")
-try:
+    try:
+        print("CRISIS ALERT: Attempting access to 'lost_archive.txt'...")
+        with open("lost_archive.txt", "r") as crisis_file:
+            print("SUCCESS: Archive located")
+    except FileNotFoundError:
+        print("RESPONSE: Archive not found in storage matrix")
+    print("STATUS: Crisis handled, system stable")
+    try:
+        print("\nCRISIS ALERT: Attempting access to 'classified_vault.txt'...")
+        with open("classified_vault.txt", "r") as vault_file:
+            print("SUCCESS: vault accessed")
+    except PermissionError:
+        print("RESPONSE: Security protocols deny access")
     print("\nROUTINE ACCESS: Attempting access to 'standard_archive.txt'...")
     with open("standard_archive.txt", "r") as standard_file:
         print(f"SUCCESS: Archive recovered - `{standard_file.read()}`")
